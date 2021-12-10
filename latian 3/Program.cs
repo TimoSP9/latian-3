032470ed1b0f6916c2d54bc285958c293f0d3056
timotius septian <tseptian@student.ciputra.ac.id>
2021-12-10 09:35:17 +0700

    Add project files.

---
+++ latian 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace latian_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("INPUT KALIMAT \t:");
+            string kalimat = Console.ReadLine();
+            Console.Write("INPUT KATA \t:");
+            var kata = Console.ReadLine();
+            Console.WriteLine("OUTPUT \t: \nSTATISTIK HURUF \t:");
+            List<string> Kata = new List<string>();
+            List<string> Kata = new List<string>();
+        }
+    }
+}
